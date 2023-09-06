7999c286c2955a37fdde78203cf7e67304309e49
Jared Bada <official.jared.bada@gmail.com>
2023-09-06 22:45:37 +0300

    project structure set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'isma-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'integrated-self-help-group-management-application';
+  title = 'adebt-free';
 }
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
+import { JwtInterceptor } from './jwt.interceptor';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  imports: [CommonModule, HttpClientModule, SharedModule],
+  providers: [
+    JwtHelperService,
+    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  ],
+  exports:[SharedModule]
+})
+export class CoreModule {}
