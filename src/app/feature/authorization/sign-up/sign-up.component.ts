17d4b78e1c5dec589c2bab7ccff828e07f931f6b
Jared Bada <56579838+architect254@users.noreply.github.com>
2023-09-11 00:29:32 +0000

    progress on sign up

--- src/app/feature/authorization/sign-up/sign-up.component.ts
+++ src/app/feature/authorization/sign-up/sign-up.component.ts
@@ -1,33 +1,69 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  NgForm,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
+import {
+  ConfirmValidParentMatcher,
+  CustomValidators,
+  errorMessages,
+  regExps,
+} from '../validators';
 
 @Component({
   selector: 'isma-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent {
-  signUpForm = this.fb.group({
-    email:[``,Validators.required,Validators.email],
-    phone:[``,Validators.required,],
-    password:[``,Validators.required],
-    confirmPassword:[``,Validators.required]
-  })
+  signUpForm!: FormGroup;
 
-  matcher = new MyErrorStateMatcher();
+  confirmValidParentMatcher: ErrorStateMatcher =
+    new ConfirmValidParentMatcher();
+  errors = errorMessages;
 
-  get email(){ return this.signUpForm.get(`email`)}
-  get phone(){ return this.signUpForm.get(`phone`)}
-  get password(){ return this.signUpForm.get(`password`)}
-  get confirmPassword(){ return this.signUpForm.get(`confirmPassword`)}
+  constructor(private fb: FormBuilder) {
+    this.buildForm();
+  }
 
-  constructor(private fb:FormBuilder){}
+  get email() {
+    return this.signUpForm.get(`email`);
+  }
+  get phone() {
+    return this.signUpForm.get(`phone`);
+  }
+  get password() {
+    return this.signUpForm.get(`password`);
+  }
+  get confirmPassword() {
+    return this.signUpForm.get(`confirmPassword`);
+  }
+
+  buildForm() {
+    this.signUpForm = this.fb.group({
+      email: [``, [Validators.required, Validators.email]],
+      phone: [``, Validators.required],
+      matchingPasswords: this.fb.group(
+        {
+          password: [
+            ``,
+            [Validators.required, Validators.pattern(regExps['password'])],
+          ],
+          confirmPassword: [``, Validators.required],
+        },
+        { validators: CustomValidators.childrenMatch }
+      ),
+    });
+  }
+
+  handleSubmit() {}
 }
